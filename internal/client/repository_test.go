11e3fd27b898e2d20fa5a8e52090d97bb5e3ced3
Raj Siva-Rajah <raj@zapzap.cloud>
2025-07-02 03:25:50 +0000

    Move uuid to separate package
    Signed-off-by: Raj Siva-Rajah <raj@zapzap.cloud>

--- internal/client/repository_test.go
+++ internal/client/repository_test.go
@@ -17,6 +17,7 @@ import (
 	"time"
 
 	"github.com/distribution/distribution/v3"
+	"github.com/distribution/distribution/v3/internal/dcontext"
 	"github.com/distribution/distribution/v3/internal/uuid"
 	"github.com/distribution/distribution/v3/manifest/ocischema"
 	"github.com/distribution/distribution/v3/registry/api/errcode"
